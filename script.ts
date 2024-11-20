726685b71c8142905b963fe036d4d7adf97ad53a
muhammad tanzeel khan <161112644+Muhammadtanzeelkhan@users.noreply.github.com>
2024-11-20 14:49:15 +0500

    Add files via upload

--- script.ts
+++ script.ts
@@ -7,12 +7,12 @@ form.addEventListener('submit', (event: Event) => {
 event.preventDefault (); // prevent page reload 
 
 // collect input values 
-const name = (document.getElementById('name') as HTMLFormElement).value
-const email = (document.getElementById('email') as HTMLFormElement).value
-const phone = (document.getElementById('phone') as HTMLFormElement).value
-const eudcation = (document.getElementById('education') as HTMLFormElement).value
-const experience = (document.getElementById('experience') as HTMLFormElement).value
-const skills = (document.getElementById('skills') as HTMLFormElement).value
+const name = (document.getElementById('name') as HTMLInputElement).value
+const email = (document.getElementById('email') as HTMLInputElement).value
+const phone = (document.getElementById('phone') as HTMLInputElement).value
+const eudcation = (document.getElementById('education') as HTMLInputElement).value
+const experience = (document.getElementById('experience') as HTMLInputElement).value
+const skills = (document.getElementById('skills') as HTMLInputElement).value
 
 // Generate the resume content dynamically 
 const resumeHTML = `
